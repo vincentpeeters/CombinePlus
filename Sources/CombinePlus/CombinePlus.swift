c99093d409d5a079c255f9a1871fe597b87092c0
Vincent <vincent.inaki.peeters@gmail.com>
2020-01-14 07:53:29 +0100

    ContractSheet not crashing anymore

--- Sources/CombinePlus/CombinePlus.swift
+++ Sources/CombinePlus/CombinePlus.swift
@@ -5,7 +5,23 @@ typealias ValueDuo<Value> = (previousValue: [Value], currentValue: [Value])
 
 @available(iOS 13.0, *)
 @available(OSX 10.15, *)
-extension Publisher {
+public extension Publisher {
+    
+    func mapLatestTwo<T>(_ closure: @escaping ( (previousOutput: Output,currentOutput: Output)) -> T) -> Publishers.CompactMap<Publishers.Scan<Self, (Self.Output?, Self.Output?)>, T>  {
+        let publisher = self.scan((nil, nil)) { (previousCouple, newOutput) -> (previousOutput: Output?,currentOutput: Output?) in
+            let previousOutput = previousCouple.1
+            let newOutput = newOutput
+            return (previousOutput, newOutput)
+        }
+        .compactMap{
+            guard let first = $0.0, let second = $0.1 else { return nil }
+            return (first, second)
+        }
+        .map {
+            closure($0)
+        }
+        return publisher
+    }
     
     func mapLatestTwo<T>(_ closure: @escaping ( (previousOutput: Output,currentOutput: Output)) -> T) -> Publishers.Map<Publishers.Scan<Self, (Output, Output)>, T> where Output: Collection, Output: ExpressibleByArrayLiteral {
         let publisher = self.scan((Output(), Output())) { (previousCouple, newOutput) -> (previousOutput: Output,currentOutput: Output) in
@@ -19,18 +35,24 @@ extension Publisher {
     }
     
     
-    func onlyInsertions<Element>() -> Publishers.Map<Publishers.Scan<Self, (Set<Element>, Set<Element>)>, Set<Element>> where Output == Set<Element> {
-        self.mapLatestTwo { (previousSet, currentSet) -> Set<Element> in
-            currentSet.filter { !previousSet.contains($0) }
+    func insertions<T>() -> Publishers.RemoveDuplicates<Publishers.Filter<Publishers.Map<Publishers.Scan<Self, (Self.Output, Self.Output)>, Set<T>>>> where Output: Collection, Output.Element == T, T: Hashable, Self.Output: ExpressibleByArrayLiteral {
+        self.mapLatestTwo { (previousSet, currentSet) -> Set<T> in
+            Set(currentSet.filter { !previousSet.contains($0) })
         }
+        .filter { !$0.isEmpty }
+        .removeDuplicates()
     }
     
-    func onlyRemovals<Element>() -> Publishers.Map<Publishers.Scan<Self, (Set<Element>, Set<Element>)>, Set<Element>> where Output == Set<Element> {
-        self.mapLatestTwo { (previousSet, currentSet) -> Set<Element> in
-            previousSet.filter { !currentSet.contains($0) }
+    
+    func removals<T>() -> Publishers.RemoveDuplicates<Publishers.Filter<Publishers.Map<Publishers.Scan<Self, (Self.Output, Self.Output)>, Set<T>>>> where Output: Collection, Output.Element == T, T: Hashable, Self.Output: ExpressibleByArrayLiteral {
+        self.mapLatestTwo { (previousSet, currentSet) -> Set<T> in
+            Set(previousSet.filter { !currentSet.contains($0) })
         }
+        .filter { !$0.isEmpty }
+        .removeDuplicates()
     }
+    
+    
+
     
 }
-
-
--- Tests/CombinePlusTests/CombinePlusTests.swift
+++ Tests/CombinePlusTests/CombinePlusTests.swift
@@ -1,15 +1,60 @@
 import XCTest
 @testable import CombinePlus
+import Combine
 
+@available(OSX 10.15, *)
 final class CombinePlusTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(CombinePlus().text, "Hello, World!")
+    
+    
+    @Published var ints: Set<Int> = []
+    @Published var int = 0
+
+    
+    override func setUp() {
+        self.int = 0
+        self.ints = []
     }
+    
+    func testRemovalAndInsertionPublisher() {
+        
+        var lastInsertions: Set<Int> = []
+        var lastRemovals: Set<Int> = []
+        
+        let insertionSubscribton = $ints
+        .insertions()
+            .sink { lastInsertions = $0; print($0) }
+        
+        let removalSubscribton = $ints
+        .removals()
+            .sink { lastRemovals = $0; print($0) }
+        
+        ints.insert(3)
+        ints.insert(3)
+        ints.insert(5)
+        ints.remove(3)
+        
+        XCTAssertEqual(lastRemovals, [3])
+        XCTAssertEqual(lastInsertions, [5])
+        
+    }
+    
+    func testMapLatestTwo() {
+        
+        var lastDuo: (Int, Int) = (0,0)
+        
+        let lastTwoSub = self.$int
+            .mapLatestTwo { $0 }
+            .sink { lastDuo = $0 }
+        int = 0
+        for i in 1...10 {
+            self.int = i
+            XCTAssertEqual(lastDuo.0, i-1)
+            XCTAssertEqual(lastDuo.1, i)
+        }
+    }
+    
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testRemovalAndInsertionPublisher", testRemovalAndInsertionPublisher),
     ]
 }
